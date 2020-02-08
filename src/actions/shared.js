e48e80b3740fb4b04415b0388456a2fe7d6074f1
chrizmichels <christian@4arge.com>
2020-02-08 10:29:01 +0100

    coding alomg

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,6 @@
-import { getInitialData } from "./utils/api";
+import { getInitialData } from "../utils/api";
 import { receiveUsers } from "../actions/users";
-import { receiveTweets } from "../actions/users";
+import { receiveTweets } from "../actions/tweets";
 import { setAuthedUser } from "../actions/authedUser";
 
 const AUTHED_ID = "tylermcginnis";
